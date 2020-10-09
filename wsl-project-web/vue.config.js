3c3b0a6ab2d613bb7fee6ee7b602c92a86b2a524
silin.wang <wsl2535620700@qq.com>
2020-10-09 16:12:37 +0800

    3000

--- wsl-project-web/vue.config.js
+++ wsl-project-web/vue.config.js
@@ -20,14 +20,14 @@ module.exports = {
       open: true,
       /* 设置为0.0.0.0则所有的地址均能访问 */
       host: '0.0.0.0',
-      port: 8088,
+      port: 8089,
       https: false,
       hotOnly: false,
       /* 使用代理 */
       proxy: {
           '/api': {
               /* 目标代理服务器地址 */
-              target: 'http://47.100.47.3/',
+              target: 'http://localhost:3000/api',
               /* 允许跨域 */
               changeOrigin: true,
           },
