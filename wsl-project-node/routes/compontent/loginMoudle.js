b2863d8c107dea3bf07f454f0c232a437b2decdd
silin.wang <wsl2535620700@qq.com>
2020-10-10 16:35:23 +0800

    路由

--- wsl-project-node/routes/compontent/loginMoudle.js
+++ wsl-project-node/routes/compontent/loginMoudle.js
@@ -1,10 +1,38 @@
+const { json } = require('express');
+
 const get_register = (req, res, next) => {
-  console.log('GET')
   res.end('HELLO GET')
 }
 const post_register = (req, res, next) => {
-  console.log('POST')
-  res.end('HELLO POST')
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({     
+    host: 'localhost',
+    user: 'root',
+    password : '1234561',
+    database: 'test_library' 
+  }); 
+
+  connection.connect();
+ 
+  var sql = 'SELECT * FROM login_information';
+  connection.query(sql,function (err, result) {
+    if(err){
+      console.log('[SELECT ERROR]: ', err.message);
+      res.end(JSON.stringify({
+        code: 500,
+        msg: '数据库请求失败'
+      }))
+      return;
+    }
+    res.end(JSON.stringify({
+      code: 0,
+      msg: 'success',
+      data: result
+    }))
+  });
+  
+  connection.end();
+
 }
 
 module.exports = {
