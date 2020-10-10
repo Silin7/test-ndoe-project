fb2959d9becf68064fca57ba3e7ad5a021020341
silin7 <wsl2535620700@qq.com>
2020-10-11 01:15:35 +0800

    注册

--- wsl-project-web/src/api/api.js
+++ wsl-project-web/src/api/api.js
@@ -1,11 +1,6 @@
 import axios from 'axios'
 import baseConfig from './baseConfig'
 axios.defaults.baseURL = baseConfig.baseURL;
-// axios.defaults.headers.post['Access-Control-Allow-Methods'] = '*';
-// axios.defaults.headers.post['Access-Control-Allow-Headers'] = 'X-Requested-With';
-// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-// axios.defaults.headers.post['X-Powered-By'] = '3.2.1';
-// axios.defaults.headers.post['Content-Type'] = '*';
 
 import api_login from './components/login'
 
