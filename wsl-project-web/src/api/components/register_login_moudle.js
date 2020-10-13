34a3b81cfe647fa186194db4189f7b2e1da57459
silin.wang <wsl2535620700@qq.com>
2020-10-13 16:49:44 +0800

    注释

--- wsl-project-web/src/api/components/register_login_moudle.js
+++ wsl-project-web/src/api/components/register_login_moudle.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 let login = {
+  // 注册：判断账号是否存在
   post_register: function (params, cb) {
     axios.post('/register/post_register', params).then((res) => {
       cb(res)
@@ -8,6 +9,7 @@ let login = {
       return Promise.reject(error)
     })
   },
+  // 注册：将注册信息写入数据库
   post_register_inster: function (params, cb) {
     axios.post('/register/post_register_inster', params).then((res) => {
       cb(res)
@@ -15,6 +17,7 @@ let login = {
       return Promise.reject(error)
     })
   },
+  // 登录：判断账号是否存在
   get_loginInfo: function (cb) {
     axios.get('/login/get_loginInfo').then((res) => {
       cb(res)
@@ -22,6 +25,7 @@ let login = {
       return Promise.reject(error)
     })
   },
+  // 登录：验证账号信息，登录
   post_login: function (params, cb) {
     axios.post('/login/post_login', params).then((res) => {
       cb(res)
